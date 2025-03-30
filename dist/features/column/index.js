99b0f5a19cacdfe37f4060aa1721d4da710b6bcd
MostafaAdly <MostafaAdly.official@gmail.com>
2025-03-31 00:53:33 +0200

    added a fixed height to the column with a scroll property of auto

--- dist/features/column/index.js
+++ dist/features/column/index.js
@@ -5,7 +5,12 @@ import { Card } from '../card';
 import { withDroppable } from '../with-droppable';
 import { CardAdder } from '../card-adder';
 import { pickPropOut } from '../../services/utils';
-const ColumnEmptyPlaceholder = forwardRef((props, ref) => (_jsx("div", Object.assign({ ref: ref, style: { minHeight: 'inherit', height: 'inherit' } }, props), void 0)));
+const ColumnEmptyPlaceholder = forwardRef(({ className, style, ...props }, ref) => (_jsx("div", Object.assign({ ref: ref, style: {
+        minHeight: 'inherit',
+        height: '500px',
+        overflowY: 'auto',
+        ...style,
+    } }, props), void 0)));
 const DroppableColumn = withDroppable(ColumnEmptyPlaceholder);
 export const Column = function ({ children: column, index: columnIndex, renderCard, renderColumnHeader, disableColumnDrag, disableCardDrag, onCardNew, allowAddCard, }) {
     return (_jsx(Draggable, Object.assign({ draggableId: `column-draggable-${column.id}`, index: columnIndex, isDragDisabled: disableColumnDrag }, { children: (columnProvided) => {
@@ -16,7 +21,7 @@ export const Column = function ({ children: column, index: columnIndex, renderCa
                     display: 'inline-block',
                     verticalAlign: 'top',
                     ...columnProvided.draggableProps.style,
-                }, className: 'react-kanban-column', "data-testid": `column-${column.id}` }, { children: [_jsx("div", Object.assign({}, columnProvided.dragHandleProps, { children: renderColumnHeader(column) }), void 0), allowAddCard && _jsx(CardAdder, { column: column, onConfirm: onCardNew }, void 0), "asdasd", _jsx(DroppableColumn, Object.assign({ droppableId: String(column.id) }, { children: column.cards.length ? (column.cards.map((card, index) => (_jsx(Card, Object.assign({ index: index, renderCard: (dragging) => renderCard(column, card, dragging), disableCardDrag: disableCardDrag }, { children: card }), card.id)))) : (_jsx("div", { className: 'react-kanban-card-skeleton' }, void 0)) }), void 0)] }), void 0));
+                }, className: 'react-kanban-column', "data-testid": `column-${column.id}` }, { children: [_jsx("div", Object.assign({}, columnProvided.dragHandleProps, { children: renderColumnHeader(column) }), void 0), allowAddCard && _jsx(CardAdder, { column: column, onConfirm: onCardNew }, void 0), _jsx(DroppableColumn, Object.assign({ droppableId: String(column.id) }, { children: column.cards.length ? (column.cards.map((card, index) => (_jsx(Card, Object.assign({ index: index, renderCard: (dragging) => renderCard(column, card, dragging), disableCardDrag: disableCardDrag }, { children: card }), card.id)))) : (_jsx("div", { className: 'react-kanban-card-skeleton' }, void 0)) }), void 0)] }), void 0));
         } }), void 0));
 };
 export * from './components';
